5209c78ba7660fa9e03f746c9bc6fd9fcc13e05b
Nir Galon <nir@galons.io>
2025-05-03 18:20:00 +0300

    fix: greptile review changes

--- src/add-subscription.tsx
+++ src/add-subscription.tsx
@@ -1,23 +1,19 @@
 import { Action, ActionPanel, Form, getPreferenceValues, showToast, Toast, useNavigation } from "@raycast/api";
-import { getStuff } from "./state";
+import { fetchSubscriptions } from "./state";
 import { NewSubscription } from "./types";
-import { FormValidation, useForm } from "@raycast/utils";
+import { FormValidation, showFailureToast, useForm } from "@raycast/utils";
 
 export default function Command() {
   const { pop } = useNavigation();
-  const { subwatchApiKey } = getPreferenceValues<Preferences>();
-  const { isLoading, data, error, mutate, revalidate } = getStuff();
+  const { subwatchApiKey, supabaseApiKey } = getPreferenceValues<Preferences>();
+  const { isLoading, data, error, mutate, revalidate } = fetchSubscriptions();
 
   if (error) {
-    showToast({
-      style: Toast.Style.Success,
-      title: "An error occurred!",
-      message: error.message,
-    });
+    showFailureToast(error, { title: "An error occurred!" });
   }
 
   async function handleCreate(subscription: NewSubscription) {
-    const toast = await showToast({ style: Toast.Style.Animated, title: `Adding ${subscription.service}` });
+    await showToast({ style: Toast.Style.Animated, title: `Adding ${subscription.service}` });
     try {
       data?.[0].data.push({
         name: subscription.service,
@@ -25,8 +21,8 @@ export default function Command() {
         count: 0,
         billing: [
           {
-            start_date: String(subscription.startDate.toISOString()),
-            end_date: String(subscription.endDate),
+            start_date: String(subscription.start_date.toISOString()),
+            end_date: String(subscription.end_date),
             price: Number(subscription.price),
             interval: subscription.interval,
             count: 0,
@@ -37,13 +33,9 @@ export default function Command() {
       await mutate(
         fetch("https://nzyzephaenhlxoohrphc.supabase.co/rest/v1/rpc/raycast_update_data", {
           method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-            apikey:
-              "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im56eXplcGhhZW5obHhvb2hycGhjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQxOTQzMjgsImV4cCI6MjA1OTc3MDMyOH0.6AboCGgJGqJMTgqUH3LsYmhoWQ8sfEWqdv0cY-1EXIg",
-          },
+          headers: { "Content-Type": "application/json", apikey: supabaseApiKey },
           body: JSON.stringify({ raycast_uuid: subwatchApiKey, newdata: data?.[0].data }),
-        }),
+        })
       );
       revalidate();
 
@@ -54,9 +46,7 @@ export default function Command() {
       });
     } catch (err) {
       // The data will automatically be rolled back to its previous value.
-      toast.style = Toast.Style.Failure;
-      toast.title = `Could not add ${subscription.service}`;
-      toast.message = err?.toString();
+      showFailureToast(err, { title: `Could not add ${subscription.service}` });
     }
   }
 
@@ -74,10 +64,12 @@ export default function Command() {
           return "Price should be a number";
         } else if (!value) {
           return "The item is required";
+        } else if (Number(value) < 1) {
+          return "Price should be a positive number";
         }
       },
       interval: FormValidation.Required,
-      startDate: FormValidation.Required,
+      start_date: FormValidation.Required,
     },
   });
 
@@ -103,8 +95,8 @@ export default function Command() {
         <Form.Dropdown.Item value="yearly" title="Yearly" />
       </Form.Dropdown>
 
-      <Form.DatePicker id="startDate" title="Start Date" />
-      <Form.DatePicker id="endDate" title="End Date" />
+      <Form.DatePicker id="start_date" title="Start Date" />
+      <Form.DatePicker id="end_date" title="End Date" />
     </Form>
   );
 }
--- src/manage-subscriptions.tsx
+++ src/manage-subscriptions.tsx
@@ -1,36 +1,29 @@
 import { Action, ActionPanel, getPreferenceValues, Icon, List, showToast, Toast } from "@raycast/api";
 
-import { getStuff } from "./state";
+import { fetchSubscriptions } from "./state";
 import CreateSubscriptionAction from "./add-subscription";
+import { showFailureToast } from "@raycast/utils";
 
 export default function Command() {
-  const { subwatchApiKey } = getPreferenceValues<Preferences>();
-  const { isLoading, data, error, mutate } = getStuff();
+  const { subwatchApiKey, supabaseApiKey } = getPreferenceValues<Preferences>();
+  const { isLoading, data, error, mutate } = fetchSubscriptions();
 
   if (error) {
-    showToast({
-      style: Toast.Style.Success,
-      title: "An error occurred!",
-      message: error.message,
-    });
+    showFailureToast(error, { title: "An error occurred!" });
   }
 
   async function handleDelete(index: number) {
     const subscriptionToDelete = data?.[0].data[index];
-    const toast = await showToast({ style: Toast.Style.Animated, title: `Deleting ${subscriptionToDelete?.name}` });
+    await showToast({ style: Toast.Style.Animated, title: `Deleting ${subscriptionToDelete?.name}` });
 
     try {
       data?.[0].data.splice(index, 1);
       await mutate(
         fetch("https://nzyzephaenhlxoohrphc.supabase.co/rest/v1/rpc/raycast_update_data", {
           method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-            apikey:
-              "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im56eXplcGhhZW5obHhvb2hycGhjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQxOTQzMjgsImV4cCI6MjA1OTc3MDMyOH0.6AboCGgJGqJMTgqUH3LsYmhoWQ8sfEWqdv0cY-1EXIg",
-          },
+          headers: { "Content-Type": "application/json", apikey: supabaseApiKey },
           body: JSON.stringify({ raycast_uuid: subwatchApiKey, newdata: data?.[0].data }),
-        }),
+        })
       );
       showToast({
         style: Toast.Style.Success,
@@ -39,9 +32,7 @@ export default function Command() {
       });
     } catch (err) {
       // The data will automatically be rolled back to its previous value.
-      toast.style = Toast.Style.Failure;
-      toast.title = `Could not add ${subscriptionToDelete?.name}`;
-      toast.message = err?.toString();
+      showFailureToast(err, { title: `Could not delete ${subscriptionToDelete?.name}` });
     }
   }
 
@@ -66,12 +57,7 @@ export default function Command() {
           actions={
             <ActionPanel>
               <ActionPanel.Section>
-                <Action.Push
-                  icon={Icon.Pencil}
-                  title="Create Subscription"
-                  shortcut={{ modifiers: ["cmd"], key: "n" }}
-                  target={<CreateSubscriptionAction />}
-                />
+                <Action.Push icon={Icon.Pencil} title="Create Subscription" shortcut={{ modifiers: ["cmd"], key: "n" }} target={<CreateSubscriptionAction />} />
                 <DeleteSubscriptionAction onDelete={() => handleDelete(index)} />
               </ActionPanel.Section>
             </ActionPanel>
@@ -88,9 +74,7 @@ export default function Command() {
                   <List.Item.Detail.Metadata.Label title="Start date" text={item.billing[0].start_date} />
                   <List.Item.Detail.Metadata.Label
                     title="End date"
-                    text={
-                      item.billing[0].end_date && item.billing[0].end_date != "null" ? item.billing[0].end_date : ""
-                    }
+                    text={item.billing[0].end_date && item.billing[0].end_date != "null" ? item.billing[0].end_date : ""}
                   />
                 </List.Item.Detail.Metadata>
               }
@@ -103,12 +87,5 @@ export default function Command() {
 }
 
 function DeleteSubscriptionAction(props: { onDelete: () => void }) {
-  return (
-    <Action
-      icon={Icon.Trash}
-      title="Delete Subscription"
-      shortcut={{ modifiers: ["ctrl"], key: "x" }}
-      onAction={props.onDelete}
-    />
-  );
+  return <Action icon={Icon.Trash} title="Delete Subscription" shortcut={{ modifiers: ["ctrl"], key: "x" }} onAction={props.onDelete} />;
 }
